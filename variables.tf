0bbd8816e682d4f2c00c971bb3ea6ff1838e9761
jpietryszak <jakub.pietryszak@gmail.com>
2024-12-09 16:22:54 +0100

    possibility to add more workers

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "number_of_workers" {
+    description = "Number of wokrker nodes"
+    type = number
+    default = 2
+}
+
 variable "ec2_master_user_data" {
   description = "EC2 user data for master node"
   type        = string
@@ -21,7 +27,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 sudo mkdir /etc/containerd/
-sudo containerd config default > /etc/containerd/config.toml
+sudo containerd config default |sudo tee /etc/containerd/config.toml
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 sudo sysctl --system
 sudo modprobe br_netfilter
@@ -52,9 +58,11 @@ sudo cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-sudo hostnamectl set-hostname worker
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg containerd
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_NAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Name)
+sudo hostnamectl set-hostname $INSTANCE_NAME
 sudo mkdir -p /etc/apt/keyrings/
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -62,7 +70,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 sudo mkdir /etc/containerd/
-sudo containerd config default > /etc/containerd/config.toml
+sudo containerd config default |sudo tee /etc/containerd/config.toml
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 sudo sysctl --system
 sudo modprobe br_netfilter
@@ -70,164 +78,87 @@ sudo systemctl restart containerd
 EEE
 }
 
-variable "sg_rules" {
-    type = list(object({
-      cidr_blocks = list(string)
-      description = string
-      from_port = number
-      ipv6_cidr_blocks = list(string)
-      prefix_list_ids = list(string)
-      protocol = string
-      security_groups = list(string)
-      self = string
-      to_port = number
-    }))
-    description = "kubernetes sec group rules"
-    default = [
+variable "ingress_sg_rules" {
+#  type = list(object({
+#    cidr_blocks      = list(string)
+#    description      = string
+#    from_port        = number
+#    ipv6_cidr_blocks = list(string)
+#    prefix_list_ids  = list(string)
+#    protocol         = string
+#    security_groups  = list(string)
+#    self             = string
+#    to_port          = number
+#  }))
+  description = "kubernetes sec group rules"
+  default = [
     {
-        cidr_blocks      = [
-            "0.0.0.0/0",
-        ]
-        description      = null
-        from_port        = 22
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = false
-        to_port          = 22
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      from_port        = 22
+      self             = false
+      to_port          = 22
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 10248
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 10248
+      from_port        = 10248
+      self             = true
+      to_port          = 10248
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 10250
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 10250
+      from_port        = 10250
+      self             = true
+      to_port          = 10250
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 10257
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 10257
+      from_port        = 10257
+      self             = true
+      to_port          = 10257
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 10259
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 10259
+      from_port        = 10259
+      self             = true
+      to_port          = 10259
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 2379
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 2380
+      from_port        = 2379
+      self             = true
+      to_port          = 2380
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 30000
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 32767
+      from_port        = 30000
+      self             = true
+      to_port          = 32767
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 443
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 443
+      from_port        = 443
+      self             = true
+      to_port          = 443
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 4443
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 4443
+      from_port        = 4443
+      self             = true
+      to_port          = 4443
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 6443
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 6443
+      from_port        = 6443
+      self             = true
+      to_port          = 6443
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 6783
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 6783
+      from_port        = 6783
+      self             = true
+      to_port          = 6783
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 6784
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 6784
+      from_port        = 6784
+      self             = true
+      to_port          = 6784
     },
     {
-        cidr_blocks      = []
-        description      = null
-        from_port        = 80
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = true
-        to_port          = 80
+      from_port        = 80
+      self             = true
+      to_port          = 80
     },
-    ]
-}   
+  ]
+}
